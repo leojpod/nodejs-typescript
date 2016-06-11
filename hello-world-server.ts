b5a8ecdf3b2415291630d61c0f565ffceb399c81
leojpod <leo.jpod@gmail.com>
2016-06-11 18:57:29 +0200

    create hello-world

--- hello-world-server.ts
+++ hello-world-server.ts
@@ -1,3 +1,18 @@
+import * as http from "http";
+import {IncomingMessage} from "http";
+import {ServerResponse} from "http";
+import {Server} from "http";
 /**
  * Created by leojpod on 2016-06-11.
  */
+
+function handleRequest(request: IncomingMessage, response: ServerResponse): void {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello World without planes\n');
+  response.end();
+};
+
+const svr: Server = http.createServer(handleRequest);
+svr.listen(8888);
+
+console.log('server started!');
